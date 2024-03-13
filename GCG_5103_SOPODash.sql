68b13e820caf70c271d83f37b2c02e4c533631ac
Service Web <sw@gssmail.com>
2024-03-13 11:07:42 -0500

    Commit changes in C:\Websites\Git\emarlow\5103\GCG_5103_SOPODash.sql

---
+++ GCG_5103_SOPODash.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_5103_SOPODash"(
+"SO" CHAR(7) NOT NULL ,
+"Line" CHAR(4) NOT NULL ,
+"Extrusion_Part" VARCHAR(20),
+"CustPO_Line" VARCHAR(30),
+"PO_NO" VARCHAR(20) NOT NULL ,
+"PO_Durdate" DATE,
+"JOB" VARCHAR(10),
+"Rate" NUMERIC(15,6),
+"Filname" VARCHAR(50),
+PRIMARY KEY ("SO", "Line", "PO_NO"));
